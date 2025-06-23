1be53d022750b648679838d88584d8f23c88eae1
Luca v. d. W. <github@luken.cc>
2025-06-23 17:58:21 +0200

    :bookmark: bump version to v1.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,12 @@
+## 1.1.0
+
+* Improved performance
+* Closest item distance no longer targets discovered items
+
 ## 1.0.1
 
-Updated credits in readme
+* Updated credits in readme
 
 ## 1.0.0
 
-Initial release
+* Initial release
--- Plugin.cs
+++ Plugin.cs
@@ -1,4 +1,4 @@
-using BepInEx;
+﻿using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
 using TMPro;
@@ -12,7 +12,7 @@ namespace MapValueTracker
     {
         private const string PLUGIN_GUID = "MapValueTracker";
         private const string PLUGIN_NAME = "MapValueTracker";
-        private const string PLUGIN_VERSION = "1.0.1";
+        private const string PLUGIN_VERSION = "1.1.0";
         private readonly Harmony harmony = new Harmony("Luken.REPO.MapValueTracker");
 
         public static new ManualLogSource Logger;
